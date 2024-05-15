0d60ebbffdceb5003e140395ea5df159535c8586
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2024-05-16 08:36:28 +1000

    progress

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,7 +9,6 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -25,13 +24,15 @@ impl ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
@@ -67,6 +68,12 @@ mod test {
             parse_pos_nonzero("not a number"),
             Err(ParsePosNonzeroError::ParseInt(_))
         ));
+    }
+
+    #[test]
+    fn test_parse_error_two() {
+        // This should not compile if PositiveNonzeroInteger's field is private
+        PositiveNonzeroInteger(0); // Uncommenting this line should cause a compilation error
     }
 
     #[test]
--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
@@ -13,7 +12,17 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    // Return None for invalid time_of_day
+    if time_of_day > 23 {
+        return None;
+    }
+
+    // Handle ice cream count based on the time of day
+    match time_of_day {
+        0..=21 => Some(5), // Before 10PM
+        22..=23 => Some(0), // 10PM or later
+        _ => None, // This case will never be reached because of the previous check
+    }
 }
 
 #[cfg(test)]
@@ -34,6 +43,6 @@ mod tests {
         // TODO: Fix this test. How do you get at the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams, Some(5));
     }
 }
