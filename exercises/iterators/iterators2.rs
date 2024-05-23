5bf8903227dc5695605c260b32368a6923990d3d
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2024-05-24 09:02:55 +1000

    78.1%

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +14,9 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            first.to_uppercase().to_string() + c.as_str()
+        }
     }
 }
 
@@ -24,7 +25,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 // Step 3.
@@ -32,7 +33,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words.iter().map(|&w| capitalize_first(w)).collect::<Vec<_>>().join("")
 }
 
 #[cfg(test)]
@@ -42,6 +43,11 @@ mod tests {
     #[test]
     fn test_success() {
         assert_eq!(capitalize_first("hello"), "Hello");
+    }
+
+    #[test]
+    fn test_success_one_letter() {
+        assert_eq!(capitalize_first("h"), "H");
     }
 
     #[test]
